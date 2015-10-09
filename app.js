3c66fc24e83c631a168584fbd72b0225408eba57
Peter Berezny <spontain@gmail.com>
2015-10-09 13:55:22 +0200

    Basic app setup

---
+++ app.js
@@ -0,0 +1,14 @@
+// app.js serves to start the application server
+"use strict";
+
+// Execute below in scope
+(function() {
+
+  var DIMENSE     = require("./lib/core")({
+        config : require("./config/instance"),
+        dir    : __dirname
+      })
+
+  // Listen to assigned port
+  DIMENSE.app.listen(DIMENSE.config.port)
+} ())
